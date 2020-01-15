8f86fe38d8355b2b365c9272bb69b02089bd7b2d
JinKanai <jin@kanaisan.com>
2020-01-15 15:29:31 +0900

    [WIP] use mysql conainer

--- example/harukabot/harukabot/settings.py
+++ example/harukabot/harukabot/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'harukabot.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'harukabot',
+        'USER': 'harukabot',
+        'PASSWORD': 'harukabot',
+        'HOST': 'db',
+        'PORT': '3306'
+        }
 }
 
 # Password validation
