016efc05bec576995d7133c6057256c9a3aa8a05
JinKanai <jin@kanaisan.com>
2020-01-16 15:37:38 +0900

    postgresql implemented

--- example/harukabot/harukabot/settings-sqlite3.py
+++ example/harukabot/harukabot/settings-sqlite3.py
@@ -74,14 +74,10 @@ WSGI_APPLICATION = 'harukabot.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'harukabot',
-        'USER': 'harukabot',
-        'PASSWORD': 'harukabot',
-        'HOST': 'db',
-        'PORT': '5432'
-    }
+       'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 # Password validation
